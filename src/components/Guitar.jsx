dab0b28267476b52617728edfc420d9b9f74b5ad
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-05 15:22:15 -0500

    Usando State Derivado Carrito esta vacio

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -15,7 +15,7 @@ const Guitar = ({ guitarra, addtoCart}) => {
                     {name}
                 </h3>
                 <p>{description}</p>
-                <p className="fw-black text-primary fs-3">{price}</p>
+                <p className="fw-black text-primary fs-3">$ {price}</p>
                 <button
                     onClick={() => {
                         addtoCart(guitarra);
